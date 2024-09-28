fda4efa3b375b4e7f6807adba09187d133411fa9
yusufjamiu <princeyusufjamiuadegbolahan@gmail.com>
2024-09-28 16:03:56 +0100

    Changes

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -268,8 +268,9 @@ const LandingPage = () => {
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
                 </svg>
               </div>
-              <h3 className="text-xl font-semibold mb-2">Peer Group</h3>
-              <p className="text-gray-600">We'll create a package with full of attention and meaningful list of tools. We want to make sure that you'll receive your package like you receive your birthday gift.</p>
+              <h3 className="text-xl font-semibold mb-2">Play to Learn</h3>
+              <p className="text-gray-600">Ilmquest combines entertainment and education, making learning enjoyable and engaging. Through interactive games and challenges, players acquire knowledge and skills while having fun.
+              .</p>
             </div>
 
             {/* Vast Library */}
@@ -279,19 +280,24 @@ const LandingPage = () => {
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
                 </svg>
               </div>
-              <h3 className="text-xl font-semibold mb-2">Vast Library</h3>
-              <p className="text-gray-600">You do not need to worry when you want to shape your knowledge. We will always unleash whatever your questions. Just click on the chat box and we will talk.</p>
+              <h3 className="text-xl font-semibold mb-2">Interactive Learning</h3>
+              <p className="text-gray-600">lmquest's immersive content enables active learning, promoting deeper understanding and retention. Players engage with interactive simulations, videos, and animations that simplify complex concepts.</p>
             </div>
 
-            {/* Growth Gamification */}
+            {/* Smart Progress */}
             <div className="flex flex-col items-center text-center w-full md:w-1/3">
               <div className="bg-orange-500 rounded-full p-4 mb-4">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 10V3L4 14h7v7l9-11h-7z" />
                 </svg>
               </div>
-              <h3 className="text-xl font-semibold mb-2">Growth Gamification</h3>
-              <p className="text-gray-600">Believe it's such cool experience until we don't want to stop from learning to you. Our team will make sure you will through smooth and friendly process.</p>
+              <h3 className="text-xl font-semibold mb-2">Smart Progress</h3>
+              <p className="text-gray-600">Ilmquest's adaptive technology adjusts to individual players' pace and skill level, ensuring:
+
+- Challenging yet achievable goals
+- Personalized learning paths
+- Continuous progress tracking
+.</p>
             </div>
           </div>
         </div>
@@ -453,7 +459,7 @@ const LandingPage = () => {
               <div>
                 <h3 className="text-lg font-semibold mb-2 text-orange-500">SOCIAL</h3>
                 <ul className="space-y-2">
-                  <li><a href="#" className="hover:underline">Instagram</a></li>
+                  <li><a href="https://instagram.com/_ilmquest" className="hover:underline">Instagram</a></li>
                   <li><a href="https://x.com/_ilmquest" className="hover:underline">Twitter</a></li>
                   <li><a href="#" className="hover:underline">LinkedIn</a></li>
                 </ul>
